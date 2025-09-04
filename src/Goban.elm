d261c42e85262143977f6580e56ac6585437a3d6
Dušan Rychnovský <dusan.rychnovsky@gmail.com>
2025-09-04 14:33:09 +0200

    Goban module - currentPlayer

--- src/Goban.elm
+++ src/Goban.elm
@@ -1,20 +1,39 @@
-module Goban exposing (Goban, Move, Color(..), Coords)
+module Goban exposing (Color(..), Coords, Goban, Move, currentPlayer)
+
 
 type alias Goban =
-    { size: Int
+    { size : Int
     , moves : List Move
     }
 
-type alias Move = 
-    { color: Color
-    , coords: Coords
+
+type alias Move =
+    { color : Color
+    , coords : Coords
     }
+
 
 type Color
     = White
     | Black
 
+
 type alias Coords =
     { row : Int
     , col : Int
     }
+
+
+currentPlayer : Goban -> Color
+currentPlayer goban =
+    case List.head (List.reverse goban.moves) of
+        Just lastMove ->
+            case lastMove.color of
+                White ->
+                    Black
+
+                Black ->
+                    White
+
+        Nothing ->
+            Black
--- tests/GobanTest.elm
+++ tests/GobanTest.elm
@@ -1,14 +1,33 @@
 module GobanTest exposing (all)
 
-import Goban exposing (..)
-
-import Test exposing (..)
 import Expect
+import Goban exposing (..)
+import Test exposing (..)
+
 
 all : Test
 all =
-    describe "Goban module"
-        [ test "hello world" <|
-            \_ ->
-                Expect.equal (.size (Goban 19 [])) 19
+    describe "Goban"
+        [ describe "currentPlayer"
+            [ test "is black when board is empty" <|
+                \_ ->
+                    Expect.equal (currentPlayer (Goban 19 [])) Black
+            , test "is white after black plays" <|
+                \_ ->
+                    let
+                        goban =
+                            Goban 19 [ { color = Black, coords = { row = 3, col = 3 } } ]
+                    in
+                    Expect.equal (currentPlayer goban) White
+            , test "is black after white plays" <|
+                \_ ->
+                    let
+                        goban =
+                            Goban 19
+                                [ { color = Black, coords = { row = 3, col = 3 } }
+                                , { color = White, coords = { row = 16, col = 16 } }
+                                ]
+                    in
+                    Expect.equal (currentPlayer goban) Black
+            ]
         ]
